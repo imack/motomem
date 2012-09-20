0d9f9691434cea16bbf26c50b3f3f7312e5dee4c
imackinn <imackinn@alumni.uwaterloo.ca>
2012-09-20 13:21:35 -0700

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Motomem::Application.config.secret_token = '302ab1a587df4b6a743e37717f0966fffa754d43f720f753ef833adcc50865b93cc3d54a7c6cbbd7f6e44366205cb2315ac57d1d07999e1481160f33e3f17448'
