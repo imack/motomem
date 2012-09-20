4f49674982c640b2d67a92ff173d398349df401b
imackinn <imackinn@alumni.uwaterloo.ca>
2012-09-20 16:00:26 -0700

    add mailer, and cronjob stuff

---
+++ app/mailers/daily_mailer.rb
@@ -0,0 +1,16 @@
+class DailyMailer < ActionMailer::Base
+  default from: "contact@placeling.com"
+
+  include Resque::Mailer
+
+  def daily(user_id, facebook_place)
+
+    user = User.find( user_id )
+
+
+    mail(:to => user.email, :subject => "How was your visit to #{facebook_place["name"]}") do |format|
+      format.html
+    end
+  end
+
+end
---
+++ config/schedule.rb
@@ -0,0 +1,12 @@
+# Use this file to easily define all of your cron jobs.
+#
+# It's helpful, but not entirely necessary to understand cron before proceeding.
+# http://en.wikipedia.org/wiki/Cron
+
+# Example:
+#
+set :output, "#{path}/log/cron.log"
+#
+every 10.minutes do
+  rake "motomem:daily"
+end
