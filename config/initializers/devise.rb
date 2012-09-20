726de6ef36772194d7d15df1661b66391a3daf24
imackinn <imackinn@alumni.uwaterloo.ca>
2012-09-20 14:26:10 -0700

    integrate omniauth right into devise

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -1,5 +1,14 @@
+require "omniauth-facebook"
 # Use this hook to configure devise mailer, warden hooks and so forth.
 # Many of these configuration options can be set straight in your model.
+
+begin
+  secret_keys = YAML.load_file("/etc/motomem/secret_keys.yml")
+rescue
+  secret_keys = YAML.load_file("#{::Rails.root.to_s}/config/secret_keys.yml")
+end
+
+
 Devise.setup do |config|
   # ==> Mailer Configuration
   # Configure the e-mail address which will be shown in Devise::Mailer,
@@ -205,7 +214,7 @@ Devise.setup do |config|
   # ==> OmniAuth
   # Add a new OmniAuth provider. Check the wiki for more information on setting
   # up on your models and hooks.
-  # config.omniauth :github, 'APP_ID', 'APP_SECRET', :scope => 'user,public_repo'
+  config.omniauth :facebook, secret_keys['facebook_app_id'], secret_keys['facebook_app_secret'], {:scope => "email, publish_actions", :client_options => {:ssl => {:ca_file => "/usr/lib/ssl/certs/ca-certificates.crt"}}}
 
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
