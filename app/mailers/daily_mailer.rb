2113443465fafa559144775c65373b0142e59c4a
Lindsay Watt <lindsayrgwatt@gmail.com>
2012-09-20 18:27:20 -0700

    Updated mailer

--- app/mailers/daily_mailer.rb
+++ app/mailers/daily_mailer.rb
@@ -1,24 +1,19 @@
 class DailyMailer < ActionMailer::Base
   default from: "contact@placeling.com"
-
+  
   include Resque::Mailer
-
+  
   def daily(user_id, facebook_place, google_place)
-
     @user = User.find( user_id )
     @facebook_place = facebook_place
     @google_place = google_place
-
-
+    
     mail(:to => @user.email, :subject => "How was your visit to #{facebook_place["name"]}?") do |format|
       format.html
     end
   end
 
-
-
   class Preview < MailView
-
     def daily
       user = User.first
       fb_place = {"id"=>"178044952237224", "name"=>"Calhouns", "location"=>{"city"=>"Vancouver", "state"=>"BC", "country"=>"Canada", "latitude"=>49.264249085313, "longitude"=>-123.17269132736}}
