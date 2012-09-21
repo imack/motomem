bdbb034ddb97b195317959eee3d87f88b7bde0f9
imackinn <imackinn@alumni.uwaterloo.ca>
2012-09-20 17:55:54 -0700

    fixup the re-login

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,6 @@ Motomem::Application.routes.draw do
 
   devise_scope :user do
     get 'sign_in', :to => 'users/sessions#new', :as => :new_user_session
-    get 'sign_out', :to => 'users/sessions#destroy', :as => :destroy_user_session
   end
 
 
